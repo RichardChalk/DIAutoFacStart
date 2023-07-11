c22b9e1ceee73868dc9a5fc9935ad49c2364ae54
Richard Chalk <barmybrit@gmail.com>
2023-07-11 09:06:28 +0200

    Add project files.

---
+++ DIAutoFacStart/INotificationService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WithDI
+{
+    public interface INotificationService
+    {
+        void NotifiyUserNameChanged(User user);
+    }
+}
---
+++ DIAutoFacStart/User.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WithDI
+{
+    public class User
+    {
+        private INotificationService _notificationService;
+        public string UserName { get; set; }
+
+        public User(string userName, INotificationService notificationService)
+        {
+            UserName = userName;
+            
+            // LOOSELY COUPLED till en Interface! :)
+            _notificationService = notificationService;
+        }
+
+        public void ChangeUserName(string newUserName)
+        {
+            UserName = newUserName;
+            _notificationService.NotifiyUserNameChanged(this);
+        }
+    }
+
+}
